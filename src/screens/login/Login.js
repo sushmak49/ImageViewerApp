c1cd8fd3faecb81abcb9aeb7a93b6370e268dbe9
sushma.k <sushma.k@metricstream.com>
2020-10-23 21:37:30 +0530

    Added form fields and login button

--- src/screens/login/Login.js
+++ src/screens/login/Login.js
@@ -1,9 +1,49 @@
 import React, { Component } from 'react';
 import '../login/Login.css';
 import Typography from '@material-ui/core/Typography';
-
+import { CardContent, FormHelperText } from '@material-ui/core';
+import Card from '@material-ui/core/Card';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
+import Button from '@material-ui/core/Button';
 
 class Login extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            username: '',
+            password: '',
+            usernameRequired: 'dispNone',
+            passwordRequired: 'dispNone',
+            incorrectInput: 'dispNone',
+        }
+    }
+
+    inputUsernameChangeHandler = (event) => {
+        this.setState({username:event.target.value});
+    }
+
+    inputPasswordChangeHandler = (event) => {
+        this.setState({password:event.target.value});
+    }
+
+    loginClickHandler = () => {
+
+        this.state.username === "" ? this.setState({ usernameRequired: "dispBlock" }) : this.setState({ usernameRequired: "dispNone" });
+        this.state.password === "" ? this.setState({ passwordRequired: "dispBlock" }) : this.setState({ passwordRequired: "dispNone" });
+
+        if (this.state.username!=="" && this.state.passwordRequired!=="") {
+            if(this.state.username=="user" && this.state.password=="passwd") {
+                console.log('you can login');
+                this.setState({incorrectInput:"dispNone"});
+            }else {
+                this.setState({incorrectInput:"dispBlock"});
+            }
+        }
+        
+    }
 
     render() {
         return (
@@ -12,6 +52,38 @@ class Login extends Component {
                     <Typography className="header-logo">
                         Image Viewer
                 </Typography>
+                </div>
+                <div className="login-card-container">
+                    <Card>
+                        <CardContent className="card-content">
+                            <Typography variant="headline" component="h2">
+                                LOGIN
+                        </Typography>
+                            <br /><br />
+                            <FormControl required>
+                                <InputLabel htmlFor="username">Username:</InputLabel>
+                                <Input id="username" username={this.state.username} type="text" onChange={this.inputUsernameChangeHandler}></Input>
+                                <FormHelperText className={this.state.usernameRequired}><span className="red">required</span></FormHelperText>
+                            </FormControl>
+                            <br /><br />
+                            <FormControl required>
+                                <InputLabel htmlFor="password">Password:</InputLabel>
+                                <Input id="password" password={this.state.password} type="password" onChange={this.inputPasswordChangeHandler}></Input>
+                                <FormHelperText className={this.state.passwordRequired}><span className="red">required</span></FormHelperText>
+                            </FormControl>
+                            <br /><br />
+                            <FormHelperText className={this.state.incorrectInput}><span className="red">Incorrect username and/or password</span></FormHelperText>
+                            <br/>
+                            <Button
+                                className="login-btn"
+                                variant="contained"
+                                color="primary"
+                                onClick={this.loginClickHandler}
+                            >
+                                LOGIN
+                            </Button>
+                        </CardContent>
+                    </Card>
                 </div>
             </div>
         )
