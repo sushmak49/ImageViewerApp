b55d1787227077aaafdfdaeac07bb43fb39b3284
sushma.k <sushma.k@metricstream.com>
2020-10-26 01:59:31 +0530

    Created Home Page and added Header

---
+++ src/screens/home/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Header from '../../common/header/Header.js';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <Header />
+            
+            </div>
+        )
+    }
+}
+
+export default Home;
