b455cb5b2fbb7c47e91b255306ffee6c78b71908
sushma.k <sushma.k@metricstream.com>
2020-10-25 22:47:45 +0530

    Added username, password, accesstoken state variables. Modified loginclick handler

--- src/screens/login/Login.js
+++ src/screens/login/Login.js
@@ -19,6 +19,12 @@ class Login extends Component {
             usernameRequired: 'dispNone',
             passwordRequired: 'dispNone',
             incorrectInput: 'dispNone',
+            accessToken : 'f5af9f51-07e6-4332-8f1a-c0c11c1e3728',
+            userInput: {
+                username : 'user',
+                password : 'passwd',
+            },
+            loggedIn : sessionStorage.getItem('access-token')==null?"false":"true",
         }
     }
 
@@ -34,16 +40,17 @@ class Login extends Component {
 
         let username =  this.state.username;
         let password = this.state.password;
-        let access_token = 'IGQVJYNmxrT2MwQmwzaHNheWNJZADAyaFZAhVmczWDFuMGZAKLTlUVjdFNHNTa1RJeFlRM3I1bVZApajd2X2ZAHTm55eDJ0RGRVS1h4ZAVBBdDJyam5ZAUm4yUWRpOHNncnpNaEo5ekw4WnBQRzBpZAWNGTEFLcm1LNlA1dTVKZAUxr';
+        let accessToken = this.state.accessToken;
 
-        username === "" ? this.setState({ usernameRequired: "dispBlock" }) : this.setState({ usernameRequired: "dispNone" });
-        password === "" ? this.setState({ passwordRequired: "dispBlock" }) : this.setState({ passwordRequired: "dispNone" });
+        username === "" ? this.setState({ usernameRequired: "dispBlock",incorrectInput: "dispNone"}) : this.setState({ usernameRequired: "dispNone" });
+        password === "" ? this.setState({ passwordRequired: "dispBlock",incorrectInput: "dispNone"}) : this.setState({ passwordRequired: "dispNone" });
 
         if (username!=="" && password!=="") {
-            if(username==="user" && password==="passwd") {
-                console.log('you can login');
-                sessionStorage.setItem('access-token',access_token);
+            if(username === this.state.userInput.username && password === this.state.userInput.password) {
+                sessionStorage.setItem('access-token',accessToken);
+                this.setState({loggedIn : true});
                 this.setState({incorrectInput:"dispNone"});
+                //Take user to HomePage
             }else {
                 this.setState({incorrectInput:"dispBlock"});
             }
@@ -54,8 +61,8 @@ class Login extends Component {
         return (
             <div>
                 <Header />
-                <div className="login-card-container">
-                    <Card>
+                <div>
+                    <Card className="login-card-container">
                         <CardContent className="card-content">
                             <Typography variant="headline" component="h2">
                                 LOGIN
