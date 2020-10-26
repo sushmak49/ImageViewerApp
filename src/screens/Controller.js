0d6d0e5bd5d2246f10fb516548ac527a255ffd25
sushma.k <sushma.k@metricstream.com>
2020-10-26 19:08:37 +0530

    Added routing to Profile component

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -1,8 +1,9 @@
 import React,{ Component } from 'react';
 import Login from './login/Login.js';
-import { BrowserRouter as Router, Redirect, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from '../screens/home/Home.js';
 import PrivateRoutes from '../common/PrivateRoutes.js';
+import Profile from '../screens/profile/Profile.js';
 
 class Controller extends Component{
     constructor(props){
@@ -18,6 +19,7 @@ class Controller extends Component{
                 <div className="main-container">
                     <Route exact path="/" render={(props) => <Login {...props} baseUrl={this.baseUrl} />}></Route>
                     <PrivateRoutes exact path='/home' component={Home} baseUrl={this.baseUrl} />
+                    <PrivateRoutes exact path='/profile' component={Profile} baseUrl={this.baseUrl} />
                 </div>
             </Router>
         )
