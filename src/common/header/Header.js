58c6d49925c01b0744f1885aea46119b659e627a
sushma.k <sushma.k@metricstream.com>
2020-10-26 10:38:09 +0530

    Added search bar and profile icon to header of home page

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
 import './Header.css';
+import Search from '@material-ui/icons/Search';
+import InputBase from '@material-ui/core/InputBase';
+import { IconButton } from '@material-ui/core';
 
 class Header extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            loggedIn: sessionStorage.getItem('access-token') == null ? false : true,
+        }
+    }
     render() {
         return (
             <div>
@@ -9,7 +19,31 @@ class Header extends Component {
                     <div className="header-logo">
                         Image Viewer
                     </div>
-                </div>
+
+                    {
+                        this.state.loggedIn === true ?
+                            (
+                                <div className="header-right">
+                                    <div className="search-box">
+                                        <Search />
+                                        <InputBase
+                                            className="search-input"
+                                            placeholder="Search..."
+                                        />
+                                    </div>
+                                     <div>
+                                         <IconButton>
+                                             <img src="https://upload.wikimedia.org/wikipedia/commons/f/f9/Phoenicopterus_ruber_in_S%C3%A3o_Paulo_Zoo.jpg" alt="Su" style={{ width: 40, height: 40, borderRadius: 50 }} />
+
+                                         </IconButton>
+                                     </div>
+                                </div>
+                    
+                     )
+                     :""
+                }
+
+            </div>
             </div>
         )
     }
