dba7a55700cedcb1397f699a4b50927c69853cb1
sushma.k <sushma.k@metricstream.com>
2020-10-26 15:23:47 +0530

    Added constructor and state variables to Home Component

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -2,11 +2,28 @@ import React, { Component } from 'react';
 import Header from '../../common/header/Header.js';
 
 class Home extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+          profile_picture: "",
+          username: "",
+          media: 0,
+          follows: 0,
+          followed_by: 0,
+          full_name: "",
+          access_token: sessionStorage.getItem("access-token"),
+          userPostsDetails: null,
+          filterPostsDetails: null,
+          addNewComment: [],
+          addNewCommentCheck: ""
+        };
+      }
+
     render() {
         return (
             <div>
                 <Header />
-            
             </div>
         )
     }
